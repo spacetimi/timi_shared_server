f31466d6d8462e822986544db44094bf96c68808
Avi Krishnan <cosmiczilch@gmail.com>
2020-04-10 23:17:16 +0530

    Add templates-writer to help with app-controller handlers that use html templates

---
+++ utils/templated_writer/templated_writer.go
@@ -0,0 +1,51 @@
+package templated_writer
+
+import (
+    "errors"
+    "github.com/spacetimi/timi_shared_server/utils/logger"
+    "html/template"
+    "io"
+)
+
+type TemplatedWriter struct {
+    _templatesFolderPath string
+
+    _templates *template.Template
+}
+
+func NewTemplatedWriter(templatesFolderPath string) *TemplatedWriter {
+    tw := &TemplatedWriter{
+        _templatesFolderPath:templatesFolderPath,
+    }
+    err := tw.parseTemplates()
+    if err != nil {
+        logger.LogFatal("error creating templated html writer: " + err.Error())
+    }
+
+    return tw
+}
+
+func (tw *TemplatedWriter) Render(writer io.Writer, templateName string, backingObject interface{}, forceReparse bool) error {
+
+    if forceReparse {
+        _ = tw.parseTemplates()
+    }
+
+    err := tw._templates.ExecuteTemplate(writer, templateName, backingObject)
+    if err != nil {
+        return errors.New("error rendering template: " + err.Error())
+    }
+
+    return nil
+}
+
+func (tw *TemplatedWriter) parseTemplates() error {
+    var err error
+    tw._templates, err = template.ParseGlob(tw._templatesFolderPath + "/*")
+    if err != nil {
+        return errors.New("error parsing templates: " + err.Error())
+    }
+
+    return nil
+}
+
