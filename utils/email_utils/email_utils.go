50e8f387df61391eb28a5d0ecd65468d5475e64b
Avi Krishnan <avi@spacetimi.com>
2020-04-26 02:35:07 +0530

    Add email_utils to send email via smtp

---
+++ utils/email_utils/email_utils.go
@@ -0,0 +1,57 @@
+package email_utils
+
+import (
+    "errors"
+    "github.com/spacetimi/timi_shared_server/utils/logger"
+    "net/smtp"
+    "strconv"
+)
+
+type Emailer struct {
+    Account EmailAccount
+    SmtpHost string
+    SmtpServerAddress string
+}
+
+type Email struct {
+    Subject string
+    Body string
+}
+
+type EmailAccount struct {
+    EmailAddress string
+    Password string
+}
+
+func NewEmailer(account EmailAccount, smtpHost string, smtpPort int) *Emailer {
+    mailer := &Emailer{}
+    mailer.SmtpHost = smtpHost
+    mailer.SmtpServerAddress = smtpHost + ":" + strconv.Itoa(smtpPort)
+    mailer.Account = account
+
+    return mailer
+}
+
+func (mailer *Emailer) SendEmail(to string, email Email) error {
+    fromAddress := mailer.Account.EmailAddress
+    password := mailer.Account.Password
+
+    msg := "From: " + fromAddress + "\n" +
+           "To: " + to + "\n" +
+           "Subject: " + email.Subject + "\n\n" +
+           email.Body
+
+    auth := smtp.PlainAuth("", fromAddress, password, mailer.SmtpHost)
+
+    err := smtp.SendMail(mailer.SmtpServerAddress, auth, fromAddress, []string{to}, []byte(msg))
+
+    if err != nil {
+        logger.LogError("error sending email" +
+                        "|to=" + to +
+                        "|from=" + fromAddress +
+                        "|error=" + err.Error())
+        return errors.New("error sending email")
+    }
+
+    return nil
+}
