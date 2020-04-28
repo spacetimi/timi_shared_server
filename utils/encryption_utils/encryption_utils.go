0bceafe23852c4081a7dfe824076cb64cd547556
Avi Krishnan <avi@spacetimi.com>
2020-04-29 01:06:31 +0530

    Add encrypt/decrypt using aes cipher

--- utils/encryption_utils/encryption_utils.go
+++ utils/encryption_utils/encryption_utils.go
@@ -1,12 +1,17 @@
 package encryption_utils
 
 import (
+    "crypto/aes"
+    "crypto/cipher"
     "crypto/md5"
+    "crypto/rand"
     "crypto/sha1"
     "encoding/base64"
     "encoding/hex"
     "errors"
+    "github.com/spacetimi/timi_shared_server/utils/string_utils"
     "golang.org/x/crypto/bcrypt"
+    "io"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,3 +52,53 @@ func Generate_sha_hash(data string) string {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+func EncryptUsingAES(data string, key string) (string, error) {
+
+    keyHash := Generate_md5_hash(key)
+    block, err := aes.NewCipher([]byte(keyHash))
+    if err != nil {
+        return "", errors.New("error creating aes cipher for encryption: " + err.Error())
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return "", errors.New("error creating gcm for cipher for encryption: " + err.Error())
+    }
+
+    nonce := make([]byte, gcm.NonceSize())
+    if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+        return "", errors.New("error preparing nonce from rand for encryption: " + err.Error())
+    }
+
+    cipherText := gcm.Seal(nonce, nonce, []byte(data), nil)
+    return string_utils.EncodeBytesAsBase64String(cipherText), nil
+}
+
+func DecryptUsingAES(data string, key string) (string, error) {
+
+    dataAsBytes, err := string_utils.DecodeBase64StringToBytes(data)
+    if err != nil {
+        return "", errors.New("error decoding secret from base 64: " + err.Error())
+    }
+
+    keyHashBytes := []byte(Generate_md5_hash(key))
+    block, err := aes.NewCipher(keyHashBytes)
+    if err != nil {
+        return "", errors.New("error creating aes cipher for decryption: " + err.Error())
+    }
+
+    gcm, err := cipher.NewGCM(block)
+    if err != nil {
+        return "", errors.New("error creating gcm for cipher for decryption: " + err.Error())
+    }
+
+    nonceSize := gcm.NonceSize()
+    nonce, cipherText := dataAsBytes[:nonceSize], dataAsBytes[nonceSize:]
+
+    plainText, err := gcm.Open(nil, nonce, cipherText, nil)
+    if err != nil {
+        return "", errors.New("error decrypting to plainText for decryption: " + err.Error())
+    }
+
+    return string(plainText), nil
+}
