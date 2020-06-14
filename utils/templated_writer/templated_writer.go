d6210748b8da3f568a6154b27a3c4b3d258f123b
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-14 16:25:15 +0530

    Templated writer now accepts variadic list of directories to parse templates from

--- utils/templated_writer/templated_writer.go
+++ utils/templated_writer/templated_writer.go
@@ -1,53 +1,60 @@
 package templated_writer
 
 import (
-    "errors"
-    "github.com/spacetimi/timi_shared_server/utils/logger"
-    "html/template"
-    "io"
+	"errors"
+	"html/template"
+	"io"
+
+	"github.com/spacetimi/timi_shared_server/utils/logger"
 )
 
 type TemplatedWriter struct {
-    _templatesFolderPath string
+	_templatesFolderPaths []string
 
-    _templates *template.Template
+	_templates *template.Template
 
-    ForceReparseTemplates bool
+	ForceReparseTemplates bool
 }
 
-func NewTemplatedWriter(templatesFolderPath string) *TemplatedWriter {
-    tw := &TemplatedWriter{
-        _templatesFolderPath:templatesFolderPath,
-    }
-    err := tw.parseTemplates()
-    if err != nil {
-        logger.LogFatal("error creating templated html writer: " + err.Error())
-    }
+func NewTemplatedWriter(templatesFolderPaths ...string) *TemplatedWriter {
+	var folderPaths []string
+	for _, folderPath := range templatesFolderPaths {
+		folderPaths = append(folderPaths, folderPath)
+	}
+	tw := &TemplatedWriter{
+		_templatesFolderPaths: folderPaths,
+	}
+	err := tw.parseTemplates()
+	if err != nil {
+		logger.LogFatal("error creating templated html writer: " + err.Error())
+	}
 
-    return tw
+	return tw
 }
 
 func (tw *TemplatedWriter) Render(writer io.Writer, templateName string, backingObject interface{}) error {
 
-    if tw.ForceReparseTemplates {
-        _ = tw.parseTemplates()
-    }
+	if tw.ForceReparseTemplates {
+		_ = tw.parseTemplates()
+	}
 
-    err := tw._templates.ExecuteTemplate(writer, templateName, backingObject)
-    if err != nil {
-        return errors.New("error rendering template: " + err.Error())
-    }
+	err := tw._templates.ExecuteTemplate(writer, templateName, backingObject)
+	if err != nil {
+		return errors.New("error rendering template: " + err.Error())
+	}
 
-    return nil
+	return nil
 }
 
 func (tw *TemplatedWriter) parseTemplates() error {
-    var err error
-    tw._templates, err = template.ParseGlob(tw._templatesFolderPath + "/*")
-    if err != nil {
-        return errors.New("error parsing templates: " + err.Error())
-    }
+	var err error
+	tw._templates = template.New("")
+	for _, templateFolderPath := range tw._templatesFolderPaths {
+		_, err = tw._templates.ParseGlob(templateFolderPath + "/*.html")
+		if err != nil {
+			return errors.New("error parsing templates: " + err.Error())
+		}
+	}
 
-    return nil
+	return nil
 }
-
