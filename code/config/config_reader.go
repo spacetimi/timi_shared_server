159b22dc0df67696fc7c99971d0fd8845280d6ee
Avi Krishnan <cosmiczilch@gmail.com>
2020-06-16 00:04:49 +0530

    Add file-util methods to copy file from source to destination, and to read in a json file as json object

--- code/config/config_reader.go
+++ code/config/config_reader.go
@@ -1,30 +1,16 @@
 package config
 
 import (
-	"encoding/json"
 	"errors"
-	"github.com/spacetimi/timi_shared_server/utils/logger"
-	"os"
+
+	"github.com/spacetimi/timi_shared_server/utils/file_utils"
 )
 
 func ReadConfigFile(filePath string, configObject interface{}) error {
-	file, err := os.Open(filePath)
+	err := file_utils.ReadJsonFileIntoJsonObject(filePath, configObject)
 	if err != nil {
-		return errors.New("error opening config file: " + err.Error())
+		return errors.New("error reading config file: " + err.Error())
 	}
-	defer func() {
-		err := file.Close()
-		if err != nil {
-			logger.LogError("error closing config file" +
-							"|file path=" + filePath +
-							"|error=" + err.Error())
-		}
-	}()
 
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&configObject)
-	if err != nil {
-		return errors.New("error decoding configuration file: " + err.Error())
-	}
 	return nil
 }
--- utils/file_utils/file_utils.go
+++ utils/file_utils/file_utils.go
@@ -2,11 +2,14 @@ package file_utils
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
-	"github.com/spacetimi/timi_shared_server/utils/logger"
+	"io"
 	"net/http"
 	"os"
 	"strconv"
+
+	"github.com/spacetimi/timi_shared_server/utils/logger"
 )
 
 func DoesFileOrDirectoryExist(path string) bool {
@@ -26,7 +29,7 @@ func ReadFileBytesFromURL(url string) ([]byte, error) {
 		err := response.Body.Close()
 		if err != nil {
 			logger.LogError("failed to close http request|url=" + url +
-							"|error=" + err.Error())
+				"|error=" + err.Error())
 		}
 	}()
 
@@ -34,14 +37,14 @@ func ReadFileBytesFromURL(url string) ([]byte, error) {
 	n, err := buffer.ReadFrom(response.Body)
 	if err != nil {
 		logger.LogError("failed reading file contents from http response|url=" + url +
-						"|error=" + err.Error())
+			"|error=" + err.Error())
 		return nil, errors.New("failed reading file")
 	}
 
 	if n != response.ContentLength {
 		logger.LogError("read file content length not equal to expected|url=" + url +
-						"|expected_length=" + strconv.FormatInt(response.ContentLength, 10) +
-			            "|actual_length=" + strconv.FormatInt(n, 10))
+			"|expected_length=" + strconv.FormatInt(response.ContentLength, 10) +
+			"|actual_length=" + strconv.FormatInt(n, 10))
 		return nil, errors.New("failed reading file")
 	}
 
@@ -56,4 +59,63 @@ func ReadFileFromURL(url string) (string, error) {
 	}
 
 	return string(fileBytes), nil
+}
+
+func CopyFile(sourcePath string, destinationPath string) error {
+	sourceFile, err := os.Open(sourcePath)
+	if err != nil {
+		return errors.New("error opening source file: " + err.Error())
+	}
+	defer func() {
+		err := sourceFile.Close()
+		if err != nil {
+			logger.LogWarning("error closing file" + "|file path=" + sourcePath + "|error=" + err.Error())
+		}
+	}()
+
+	destinationFile, err := os.Create(destinationPath)
+	if err != nil {
+		return errors.New("error creating destination file: " + err.Error())
+	}
+	defer func() {
+		err := destinationFile.Close()
+		if err != nil {
+			logger.LogWarning("error closing file" + "|file path=" + destinationPath + "|error=" + err.Error())
+		}
+	}()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return errors.New("error copying source to destination: " + err.Error())
+	}
+
+	err = destinationFile.Close()
+	if err != nil {
+		return errors.New("error closing destination file after copy: " + err.Error())
+	}
+
+	return nil
+}
+
+func ReadJsonFileIntoJsonObject(filePath string, jsonObject interface{}) error {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return errors.New("error opening json file: " + err.Error())
+	}
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			logger.LogError("error closing json file" +
+				"|file path=" + filePath +
+				"|error=" + err.Error())
+		}
+	}()
+
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&jsonObject)
+	if err != nil {
+		return errors.New("error decoding json file: " + err.Error())
+	}
+	return nil
+
 }
