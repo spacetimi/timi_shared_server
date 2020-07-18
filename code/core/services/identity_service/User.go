ca111fb79bf4313619df5d283bdc6c627a686bcf
Avi Krishnan <cosmiczilch@gmail.com>
2020-07-18 23:44:50 +0530

    Add GetVersion() to IBlob and use it in redis-key

--- code/core/services/identity_service/User.go
+++ code/core/services/identity_service/User.go
@@ -1,45 +1,48 @@
 package identity_service
 
 import (
-    "context"
-    "errors"
-    "github.com/spacetimi/timi_shared_server/code/config"
-    "github.com/spacetimi/timi_shared_server/code/core/services/storage_service"
-    "github.com/spacetimi/timi_shared_server/code/core/services/storage_service/storage_typedefs"
+	"context"
+	"errors"
+
+	"github.com/spacetimi/timi_shared_server/code/config"
+	"github.com/spacetimi/timi_shared_server/code/core/services/storage_service"
+	"github.com/spacetimi/timi_shared_server/code/core/services/storage_service/storage_typedefs"
 )
+
+const kUBVersion = 1
 
 // Implements IBlob
 type UserBlob struct {
-    UserId int64
-    CreatedTime int64
-    LastLoginTime int64
+	UserId        int64
+	CreatedTime   int64
+	LastLoginTime int64
 
-    UserName string
-    UserEmailAddress string
-    UserEmailAddressVerified bool
+	UserName                 string
+	UserEmailAddress         string
+	UserEmailAddressVerified bool
 
-    storage_typedefs.BlobDescriptor `bson:"ignore"`
+	storage_typedefs.BlobDescriptor `bson:"ignore"`
 }
 
 func newUserBlob(userId int64) *UserBlob {
-    user := UserBlob {
-        UserId:userId,
-    }
-    user.BlobDescriptor = storage_typedefs.NewBlobDescriptor(storage_typedefs.STORAGE_SPACE_SHARED,
-                                                             config.GetAppName() + "::user",
-                                                             []string { "UserId" },
-                                                             true)
-    return &user
+	user := UserBlob{
+		UserId: userId,
+	}
+	user.BlobDescriptor = storage_typedefs.NewBlobDescriptor(storage_typedefs.STORAGE_SPACE_SHARED,
+		config.GetAppName()+"::user",
+		[]string{"UserId"},
+		kVersion,
+		true)
+	return &user
 }
 
 func loadUserBlobByUserId(userId int64, ctx context.Context) (*UserBlob, error) {
-    user := newUserBlob(userId)
+	user := newUserBlob(userId)
 
-    err := storage_service.GetBlobByPrimaryKeys(user, ctx)
-    if err != nil {
-        return nil, errors.New("error getting user blob: " + err.Error())
-    }
+	err := storage_service.GetBlobByPrimaryKeys(user, ctx)
+	if err != nil {
+		return nil, errors.New("error getting user blob: " + err.Error())
+	}
 
-    return user, nil
+	return user, nil
 }
-
--- code/core/services/identity_service/UserEmailToIdMapping.go
+++ code/core/services/identity_service/UserEmailToIdMapping.go
@@ -1,40 +1,43 @@
 package identity_service
 
 import (
-    "context"
-    "errors"
-    "github.com/spacetimi/timi_shared_server/code/config"
-    "github.com/spacetimi/timi_shared_server/code/core/services/storage_service"
-    "github.com/spacetimi/timi_shared_server/code/core/services/storage_service/storage_typedefs"
+	"context"
+	"errors"
+
+	"github.com/spacetimi/timi_shared_server/code/config"
+	"github.com/spacetimi/timi_shared_server/code/core/services/storage_service"
+	"github.com/spacetimi/timi_shared_server/code/core/services/storage_service/storage_typedefs"
 )
+
+const kVersion = 1
 
 // Implements IBlob
 type UserEmailToIdMappingBlob struct {
-    UserEmailAddress string
-    UserId int64
+	UserEmailAddress string
+	UserId           int64
 
-    storage_typedefs.BlobDescriptor `bson:"ignore"`
+	storage_typedefs.BlobDescriptor `bson:"ignore"`
 }
 
 func newUserEmailToIdMapping(userEmailAddress string) *UserEmailToIdMappingBlob {
-    ueidm := UserEmailToIdMappingBlob{
-        UserEmailAddress:userEmailAddress,
-    }
-    ueidm.BlobDescriptor = storage_typedefs.NewBlobDescriptor(storage_typedefs.STORAGE_SPACE_SHARED,
-                                                             config.GetAppName() + "::ueidm",
-                                                             []string{ "UserEmailAddress" },
-                                                             true)
-    return &ueidm
+	ueidm := UserEmailToIdMappingBlob{
+		UserEmailAddress: userEmailAddress,
+	}
+	ueidm.BlobDescriptor = storage_typedefs.NewBlobDescriptor(storage_typedefs.STORAGE_SPACE_SHARED,
+		config.GetAppName()+"::ueidm",
+		[]string{"UserEmailAddress"},
+		kUBVersion,
+		true)
+	return &ueidm
 }
 
 func loadUserEmailToIdMappingByUserEmail(userEmailAddress string, ctx context.Context) (*UserEmailToIdMappingBlob, error) {
-    ueidm := newUserEmailToIdMapping(userEmailAddress)
+	ueidm := newUserEmailToIdMapping(userEmailAddress)
 
-    err := storage_service.GetBlobByPrimaryKeys(ueidm, ctx)
-    if err != nil {
-        return nil, errors.New("error getting ueidm blob: " + err.Error())
-    }
+	err := storage_service.GetBlobByPrimaryKeys(ueidm, ctx)
+	if err != nil {
+		return nil, errors.New("error getting ueidm blob: " + err.Error())
+	}
 
-    return ueidm, nil
+	return ueidm, nil
 }
-
